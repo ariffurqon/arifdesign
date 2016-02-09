b77e7b522053e70e9f0a6a8dba6bd6c9f1a78287
ariffurqon <arif.furqon@gmail.com>
2016-02-09 14:29:55 -0800

    facelift

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('arifdesign', ['ngRoute']);
+var app = angular.module('arifdesign', ['ngRoute', 'ngAnimate']);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
@@ -24,4 +24,16 @@ app.config(['$routeProvider', function ($routeProvider) {
 }]);
 
 
-app.controller('MainCtrl', function () {});
+app.controller('MainCtrl', function ($scope) {
+    $scope.pageClass = 'page-effect';
+});
+
+app.controller("dataImagesWork", function ($scope) {
+    $scope.images_work = [
+          { num: 1, category: 'webapp', src: "dlish.png", description: 'A web application for curating the best foodie adventures built using Angular.js frameworks with Firebase.', url_details: "details.html" },
+          { num: 2, category: 'desktopapp', src: "spider.png", description: 'desktop app ', url_details: "details.html" },
+          { num: 3, category: 'mobileapp', src: "biblio.png", description: 'mobile app', url_details: "details.html" }];
+
+
+});
+
